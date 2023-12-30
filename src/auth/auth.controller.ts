004da56711bceebf2b05244dc78eee0f860ffa44
Supanut1911 <ynotmela@gmail.com>
2023-12-30 22:55:00 +0700

    authen via session and cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './strategy/local-auth.guard';
+import { AuthenticateGuard } from './strategy/authenticated.guard';
 
 @Controller('auth')
 export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return req.user;
+    return { msg: 'login' };
+  }
+
+  @UseGuards(AuthenticateGuard)
+  @Get('protected')
+  async protected() {
+    return 'hello world';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    //for dev env this session will save in memory, but in production need to store in redis or db
+    session({
+      secret: 'cat secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 3600000 },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   await app.listen(4000);
 }
 bootstrap();
