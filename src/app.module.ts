025b6293e89028109ed28bf43530a2dea5914acc
Supanut1911 <ynotmela@gmail.com>
2023-12-30 15:48:23 +0700

    install dotenv and connfig connect mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(configService.getMongoConfig())],
   controllers: [AppController],
   providers: [AppService],
 })
